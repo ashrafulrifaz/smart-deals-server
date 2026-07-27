07b3aa6c711f4088e6d3460aa556b8cbaf494e12
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-27 18:34:00 +0600

    project setup completed

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient } = require('mongodb')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
+
+const uri = `mongodb+srv://demoUser:7LkbTwzV6voIQa1d@cluster0.klq4o7m.mongodb.net/?appName=Cluster0`
+
+const client = new MongoClient(uri)
+
+async function connectDB () {
+    try {
+        await client.connect()
+
+        const smartDB = client.db('smartDB')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+connectDB()
+
+app.listen(port, () => {
+    console.log(`example port listening on ${port}`)
+})
