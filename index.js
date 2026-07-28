c324b6e09e04a571e387009092c6fcc741248fcb
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-07-28 16:29:41 +0600

    added user post route

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient } = require('mongodb')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+app.use(cors());
+app.use(express.json());
 
-const uri = `mongodb+srv://demoUser:7LkbTwzV6voIQa1d@cluster0.klq4o7m.mongodb.net/?appName=Cluster0`
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
-const client = new MongoClient(uri)
+const uri = "mongodb+srv://demoUser:7LkbTwzV6voIQa1d@cluster0.klq4o7m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-async function connectDB () {
+const client = new MongoClient(uri);
+
+async function connectDB() {
     try {
-        await client.connect()
+        await client.connect();
 
-        const smartDB = client.db('smartDB')
-        const productCollection = db.collection('products')
+        const smartDB = client.db("smartDB");
+        const productCollection = smartDB.collection("products");
+        const usersCollection = smartDB.collection('users')
 
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = {email: user.email}
+            const isExist = await usersCollection.findOne(query)
+            if(isExist) {
+                res.send({success: false})
+            } else {
+                const result = await usersCollection.insertOne(user)
+                res.send(result)
+            }
+        })
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
 }
 
-connectDB()
+connectDB();
 
 app.listen(port, () => {
-    console.log(`example port listening on ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
